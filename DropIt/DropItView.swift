76a8c5eab58dfa78bbfb08ca78902033cd73d3c1
Başar Oğuz <baoguz@gmail.com>
2017-01-26 16:41:46 +0200

    Add UIKitExtensions and initialize drop sizes.

---
+++ DropIt/DropItView.swift
@@ -0,0 +1,25 @@
+//
+//  DropItView.swift
+//  DropIt
+//
+//  Created by Başar Oğuz on 26/01/17.
+//  Copyright © 2017 basaroguz. All rights reserved.
+//
+
+import UIKit
+
+class DropItView: UIView {
+
+    
+    private let dropsPerRow = 10
+    
+    private var dropSize: CGSize {
+        let size = bounds.size.width / CGFloat(dropsPerRow)
+        return CGSize(width: size, height: size)
+    }
+
+    func addDrop() {
+        var frame = CGRect(origin: CGPoint.zero, size: dropSize)
+        frame.origin.x
+    }
+}
